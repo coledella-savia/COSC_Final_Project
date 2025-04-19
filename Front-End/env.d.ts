3ad4e76dd38777ed14f516af3ed13a4bb3d3c0f4
Victor <volojo@proton.me>
2025-04-19 11:57:04 -0400

    updated favicon

--- Front-End/env.d.ts
+++ Front-End/env.d.ts
@@ -1,1 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASEURL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 VUE_APP_BASEURL = "http://0.0.0.0:8000";
