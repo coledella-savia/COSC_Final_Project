ac4f275c65d6954f049ccf791c7b5ef36cbe161a
Victor <volojo@proton.me>
2025-04-19 18:10:18 -0400

    fixed header redirect to dashboard

--- Front-End/src/router/index.ts
+++ Front-End/src/router/index.ts
@@ -28,6 +28,10 @@ const router = createRouter({
   history: createWebHistory("/"),
   routes: [
     {
+      path: "/",
+      redirect: "/dashboard"
+    },
+    {
       path: "/dashboard",
       name: "Dashboard",
       component: () => import("../views/DashboardView.vue"),
